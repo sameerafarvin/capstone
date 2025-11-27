172dd7b98bb80157c3b57bd65fb3c73121cc27cd
sameerafarvin <sameeraismaaeel@gmail.com>
2025-11-27 12:43:34 +0000

    added html pages

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome to the Food Bazaar!")
+    #return HttpResponse("Welcome to the Food Bazaar!")
+    return render(request, 'shop/index.html')
+
+
+def register(request):
+    return render(request, 'shop/register.html')
+   
