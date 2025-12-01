8e25beeda7f40477e309118e771515e228d4bfc0
sameerafarvin <sameeraismaaeel@gmail.com>
2025-12-01 15:59:26 +0000

    added templates

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from.models import *
 from django.contrib import messages
+
 
 
 # Create your views here.
@@ -9,7 +10,26 @@ def home(request):
     #return HttpResponse("Welcome to the Food Bazaar!")
     category=Category.objects.filter(status=0)
     return render(request,"shop/index.html",{"category":category})
+
+def collectionsview(request,name):
+    if(Category.objects.filter(name=name,status=0)):
+        products=Product.objects.filter(category__name=name)
+        return render(request,"shop/products/product.html",{"products":products,"category_name":name})
+    else:
+        messages.warning(request,"No Such Category Found")
+        return redirect('home')
     
+def product_details(request,cname,pname):
+    if(Category.objects.filter(name=cname,status=0)):
+        if(Product.objects.filter(name=pname,status=0)):
+            products=Product.objects.filter(name=pname,status=0).first()
+            return render(request,"shop/products/product_details.html",{"products":products})
+        else:
+            messages.error(request,"No Such Produtct Found")
+            return redirect('home')
+    else:
+        messages.error(request,"No Such Catagory Found")
+        return redirect('home')
 
 
 def register(request):
