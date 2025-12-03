945235c11a05160edad29ab100e9a58c1c694e99
sameerafarvin <sameeraismaaeel@gmail.com>
2025-12-03 16:23:36 +0000

    added registration,login,logout

--- shop/urls.py
+++ shop/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<str:name>',views.collectionsview,name='home'),
-    path('<str:cname>/<str:pname>',views.product_details,name="product_details"),
     path('register/', views.register, name='register'),
+    path('login/', views.login_page, name='login'),
+    path('logout/',views.logout_page,name='logout'),
+    # dynamic url routing
+    path('<str:name>/',views.collections,name='product'),
+    path('<str:cname>/<str:pname>/',views.product_details,name="product_details"),
     
 ]
--- shop/views.py
+++ shop/views.py
@@ -3,16 +3,55 @@ from django.http import HttpResponse
 from.models import *
 from django.contrib import messages
 from .form import CustomUserForm
+from django.contrib.auth import authenticate, login, logout
 
 
 
 # Create your views here.
+
 def home(request):
     #return HttpResponse("Welcome to the Food Bazaar!")
     category=Category.objects.filter(status=0)
     return render(request,"shop/index.html",{"category":category})
 
-def collectionsview(request,name):
+def register(request):
+    form=CustomUserForm()
+    if request.method=='POST':
+        form=CustomUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Registration Success ...")
+            return redirect('/login/')
+    return render(request,"shop/register.html",{'form':form})
+
+
+
+def login_page(request):
+    if request.user.is_authenticated:
+        messages.success(request,"Already user is logged in")
+        return redirect("/")
+    else:
+        if request.method=='POST':
+            name=request.POST.get('username')
+            pwd=request.POST.get('password')
+            user=authenticate(request,username=name,password=pwd)
+            if user is not None:
+                login(request,user)
+                messages.success(request,"Logged in Successfully")
+                return redirect("/")
+            else:
+                messages.error(request,"Invalid User Name or Password")
+                return redirect("/login/")
+    return render(request,"shop/login.html")
+
+
+def logout_page(request):
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(request,"Logged out Successfully")
+    return redirect("/")
+
+def collections(request,name):
     if(Category.objects.filter(name=name,status=0)):
         products=Product.objects.filter(category__name=name)
         return render(request,"shop/products/product.html",{"products":products,"category_name":name})
@@ -34,13 +73,5 @@ def product_details(request,cname,pname):
 
 
 
-def register(request):
-    form=CustomUserForm()
-    if request.method=='POST':
-        form=CustomUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request,"Registration Success Please Login")
-            return redirect('/login')
-    return render(request,"shop/register.html",{'form':form})
+
    
